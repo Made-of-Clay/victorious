836e2d879afa58877678bd0dd3045df5bab3b207
Adam <adam.m.leis@gmail.com>
2020-04-09 10:42:46 -0400

    update title; pending css extract
    will bundle css into file

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,43 @@
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
+    "transpileDependencies": [
+        "vuetify"
+    ],
+    configureWebpack: {
+        // TODO get back to this later and extract CSS properly - errors now :-/
+        // optimization: {
+        //     splitChunks: {
+        //         cacheGroups: {
+        //             styles: {
+        //                 name: 'styles',
+        //                 test: /\.css$/,
+        //                 chunks: 'all',
+        //                 enforce: true,
+        //             },
+        //         },
+        //     },
+        // },
+        // plugins: [
+        //     new MiniCssExtractPlugin({
+        //         filename: '[name].css?[hash]',
+        //     }),
+        // ],
+        // module: {
+        //     rules: [
+        //         {
+        //         test: /\.css$/,
+        //         use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        //         },
+        //     ],
+        // },
+    },
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = 'Victorious - The Victory Tracker';
+                return args;
+            });
+    }
 }
