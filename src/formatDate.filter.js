c4b257d7d7377886d25c73538d2b40d92df6584b
Adam <adam.m.leis@gmail.com>
2020-04-12 22:28:54 -0400

    fix date filter bug

--- src/formatDate.filter.js
+++ src/formatDate.filter.js
@@ -1,7 +1,7 @@
 import twoDigits from './twoDigits';
 
-export default function formatDate(milliseconds) {
-    const d = new Date(milliseconds);
+export default function formatDate(dateStr) {
+    const d = new Date(`${dateStr} 00:00:00`);
     const month = twoDigits(d.getMonth() + 1);
     const day = twoDigits(d.getDate());
     return `${month}-${day}-${d.getFullYear()}`;
